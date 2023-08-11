8c5b004e134b78ec5c17b1f18bf51183574022d4
myanpetra99 <myanpetra99@gmail.com>
2023-08-12 01:34:12 +0700

    change gin route group

--- api/index.go
+++ api/index.go
@@ -14,7 +14,7 @@ import (
 
 var app *gin.Engine
 
-func Routes(r *gin.RouterGroup) {
+func Routes(r *gin.Engine) {
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "This is home page")
 	})
@@ -25,7 +25,7 @@ func Routes(r *gin.RouterGroup) {
 		}
 		GetOriginalURL(c, db)
 	})
-	r.POST("/api/shorten", func(c *gin.Context) {
+	r.POST("/shorten", func(c *gin.Context) {
 		db, err := ConnectToDB()
 		if err != nil {
 			log.Fatalf("Unable to connect to database: %v", err)
@@ -36,8 +36,7 @@ func Routes(r *gin.RouterGroup) {
 
 func init() {
 	app = gin.New()
-	r := app.Group("/api")
-	Routes(r)
+	Routes(app)
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
