6964660edaedfcba76f61bcb6d497d4a41855f51
myanpetra99 <myanpetra99@gmail.com>
2023-08-11 15:33:20 +0700

    test

--- api/main.go
+++ api/main.go
@@ -3,36 +3,54 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	. "github.com/tbxark/g4vercel"
 )
 
-/*
-func Handler() {
-
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "This is home page")
-	})
-
-	r.GET("/:short", func(c *gin.Context) {
-		db, err := config.ConnectToDB()
-		if err != nil {
-			log.Fatalf("Unable to connect to database: %v", err)
+func Handler(w http.ResponseWriter, r *http.Request) {
+	server := New()
+	server.Use(Recovery(func(err interface{}, c *Context) {
+		if httpError, ok := err.(HttpError); ok {
+			c.JSON(httpError.Status, H{
+				"message": httpError.Error(),
+			})
+		} else {
+			message := fmt.Sprintf("%s", err)
+			c.JSON(500, H{
+				"message": message,
+			})
 		}
-		controller.GetOriginalURL(c, db)
+	}))
+	server.GET("/", func(context *Context) {
+		context.JSON(200, H{
+			"message": "OK",
+		})
 	})
-
-	r.POST("/api/shorten", func(c *gin.Context) {
-		db, err := config.ConnectToDB()
-		if err != nil {
-			log.Fatalf("Unable to connect to database: %v", err)
+	server.GET("/hello", func(context *Context) {
+		name := context.Query("name")
+		if name == "" {
+			context.JSON(400, H{
+				"message": "name not found",
+			})
+		} else {
+			context.JSON(200, H{
+				"data": fmt.Sprintf("Hello %s!", name),
+			})
 		}
-		controller.CreateNewShortURL(c, db)
 	})
-
-	r.Run(":8080")
-}
-*/
-
-func VercelHandler(w http.ResponseWriter, r http.Request) {
-	fmt.Fprintf(w, "Hello, Vercel!")
+	server.GET("/user/:id", func(context *Context) {
+		context.JSON(400, H{
+			"data": H{
+				"id": context.Param("id"),
+			},
+		})
+	})
+	server.GET("/long/long/long/path/*test", func(context *Context) {
+		context.JSON(200, H{
+			"data": H{
+				"url": context.Path,
+			},
+		})
+	})
+	server.Handle(w, r)
 }
